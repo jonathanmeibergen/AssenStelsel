877c00148cadc32fdfb365261d956a5dcd32e411
Jonathan Meibergen <jonathanmeibergen@outlook.com>
2020-06-17 18:04:54 +0200

    Text Geometry

--- AssenStelsel/AssenStelsel/MainWindow.xaml.cs
+++ AssenStelsel/AssenStelsel/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,6 +40,7 @@ namespace AssenStelsel
 
             Point pt = Mouse.GetPosition(mainCanvas);
             PathGeometry axis = new PathGeometry();
+            Geometry textGeom;
 
             LineGeometry xAxis = new LineGeometry();
             xAxis.StartPoint = new Point(0, pt.Y);
@@ -51,15 +53,57 @@ namespace AssenStelsel
             axis.AddGeometry(xAxis);
             axis.AddGeometry(yAxis);
 
-            Path xPath = new Path();
-            xPath.Stroke = Brushes.Black;
-            xPath.StrokeThickness = 1;
-            xPath.Data = axis;
+            Path axisPath = new Path();
+            axisPath.Stroke = Brushes.Black;
+            axisPath.StrokeThickness = 1;
+            axisPath.Data = axis;
+
+            string testString = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor";
+
+            // Create the initial formatted text string.
+            FormattedText formattedText = new FormattedText(
+                testString,
+                CultureInfo.GetCultureInfo("en-us"),
+                FlowDirection.LeftToRight,
+                new Typeface("Verdana"),
+                16,
+                Brushes.Black,
+                VisualTreeHelper.GetDpi(this).PixelsPerDip);
+
+            // Set a maximum width and height. If the text overflows these values, an ellipsis "..." appears.
+            formattedText.MaxTextWidth = 300;
+            formattedText.MaxTextHeight = 240;
+
+            // Use a larger font size beginning at the first (zero-based) character and continuing for 5 characters.
+            // The font size is calculated in terms of points -- not as device-independent pixels.
+            //formattedText.SetFontSize(36 * (96.0 / 72.0), 0, 5);
+
+            // Use a Bold font weight beginning at the 6th character and continuing for 11 characters.
+            formattedText.SetFontWeight(FontWeights.Bold, 6, 11);
+
+            // Use a linear gradient brush beginning at the 6th character and continuing for 11 characters.
+            //formattedText.SetForegroundBrush(Brushes.Black);
+
+            // Use an Italic font style beginning at the 28th character and continuing for 28 characters.
+            formattedText.SetFontStyle(FontStyles.Italic, 28, 28);
+
+            // Draw the formatted text string to the DrawingContext of the control.
+            textGeom = formattedText.BuildGeometry(new Point(5, 5));
+
+            Path textPath = new Path();
+            textPath.Fill = Brushes.Black;
+            textPath.Data = textGeom;
 
             mainCanvas.Children.Clear();
-            mainCanvas.Children.Add(xPath);
+            mainCanvas.Children.Add(axisPath);
+            mainCanvas.Children.Add(textPath);
 
             ptM = pt;
+        }
+
+        private void drawGrid()
+        {
+
         }
 
         private void MainWindow_MouseMove(object sender, MouseEventArgs e)
--- AssenStelsel/AssenStelsel/Punt.cs
+++ AssenStelsel/AssenStelsel/Punt.cs
@@ -11,10 +11,30 @@ namespace AssenStelsel
     {
         private double ScreenX { get; set; }
         private double ScreenY { get; set; }
-
         private double CartesianX { get; set; }
         private double CartesianY { get; set; }
+        private int rasterX { get; set; }
+        private int rasterY { get; set; }
+        private int color { get; set; }
+        private int thickness { get; set; }
+        private int colorBorder { get; set; }
+        private int thicknessBorder { get; set; }
+        public string ScreenCoordinatesAsString {
+            get { return String.Format("screen X: {0}, screen Y: {1}", ScreenX, ScreenY); }
+        }
+        public string CartesianCoordinatesAsString
+        {
+            get { return String.Format("Cartesian X: {0}, Cartesian Y: {1}", CartesianX, CartesianY); }
+        }
+        public string AllCoordinatesAsString
+        {
+            get { return ScreenCoordinatesAsString + ". " + CartesianCoordinatesAsString; }
+        }
 
+        public override string ToString()
+        {
+            return ScreenCoordinatesAsString;
+        }
 
 
     }
